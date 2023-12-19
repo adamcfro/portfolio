b426b8689326915e82621ca4a541b19c29c324a7
Adam Froehlich <adamcfro@gmail.com>
2023-12-18 20:31:36 -0800

    feat(animation): add highlight effect for various elements

--- app.js
+++ app.js
@@ -12,6 +12,63 @@ function toggleNav() {
 }
 
 /**
+ * Removes scroll event listener and adds highlight animation to skills.
+ */
+function addHighlightToSkills() {
+  if (document.documentElement.scrollTop >= 600) {
+    document.removeEventListener("scroll", addHighlightToSkills);
+    const list = document.querySelectorAll(".skills-info span");
+    for (let i = 0; i < list.length; i++) {
+      list[i].classList.add("highlight-1");
+    }
+  }
+}
+
+/**
+ * Removes scroll event listener and add highlight to projects.
+ */
+function addHighlightToProjects() {
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+
+  if (
+    document.documentElement.scrollTop >= 1500 ||
+    (viewportWidth < 651 && document.documentElement.scrollTop >= 900)
+  ) {
+    window.removeEventListener("scroll", addHighlightToProjects);
+    document.querySelector(".projects-header").classList.add("highlight-1");
+  }
+}
+
+/**
+ * Removes scroll event listener and adds an arrow to contact section. Also
+ * highlights "Get in Touch".
+ */
+function addArrowAndHighlight() {
+  const viewportWidth = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+
+  if (
+    document.documentElement.scrollTop >= 2600 ||
+    (viewportWidth < 651 && document.documentElement.scrollTop >= 2000)
+  ) {
+    window.removeEventListener("scroll", addArrowAndHighlight);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", "img/arrow.gif");
+    img.setAttribute("alt", "arrow");
+    img.classList.add("arrow");
+    document.querySelector(".get-in-touch").appendChild(img);
+
+    document.querySelector(".get-in-touch p").classList.add("highlight-1");
+  }
+}
+
+/**
  * Function to add event listeners and start the application.
  */
 function startApp() {
@@ -24,14 +81,11 @@ function startApp() {
     .querySelector(".hamburger-icon")
     .addEventListener("click", toggleNav);
 
-  document.addEventListener("scroll", (e) => {
-    if (document.documentElement.scrollTop >= 600) {
-      document.querySelector(".skills-info").style.animationName = "fadeIn";
-      setTimeout(() => {
-        document.querySelector(".skills-info").style.opacity = 1;
-      }, 2000);
-    }
-  });
+  document.addEventListener("scroll", addHighlightToSkills);
+
+  document.addEventListener("scroll", addHighlightToProjects);
+
+  window.addEventListener("scroll", addArrowAndHighlight);
 }
 
 startApp();
